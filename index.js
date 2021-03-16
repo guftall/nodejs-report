0491a41790b44063c00364371039d4901afb0a3b
oMid <dehgkaniomid@gmail.com>
2021-03-16 09:05:53 +0330

    init

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const jsreport = require('jsreport-core')()
+const app = express();
+
+jsreport.init().then(() => {
+
+    app.get('/', (req, res) => {
+        jsreport.render({
+            template: {
+                content: '<h1>Hello {{foo}}</h1>',
+                engine: 'handlebars',
+                recipe: 'chrome-pdf'
+            },
+            data: {
+                foo: "world"
+            }
+        }).then((resp) => {
+            // prints pdf with headline Hello world
+            res.end(resp.content)
+            console.log(resp.content.toString())
+        });
+    });
+}).catch((e) => {
+    console.error(e)
+})
+
+
+const server = app.listen(3000);

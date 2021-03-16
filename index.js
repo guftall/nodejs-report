dce2dc052ae20cb878fa6ce74e02c650beb5dbb5
oMid <dehgkaniomid@gmail.com>
2021-03-16 10:03:33 +0330

    e

--- index.js
+++ index.js
@@ -1,30 +1,15 @@
 const express = require('express');
 const { spawn } = require('child_process');
 
-const jsReport = spawn('./jsreport',
-    ['report',
-        `--out=${Date.now()}.pdf`,
-        '--template.name=daily-report-template.html']);
-
-jsReport.stdout.on('data', (data) => {
-    console.log(data);
-});
-
-jsReport.stderr.on('data', (data) => {
-    console.log(data);
-});
-
-jsReport.on('close', (code) => {
-    console.log(`child process exited with code ${code}`);
-});
-
 const app = express();
 
 app.get('/api/report-service', (req, res) => {
     const jsReport = spawn('./jsreport',
-        ['report',
+        ['render',
             `--out=${Date.now()}.pdf`,
-            '--template.name=daily-report-template.html']);
+            '--template.engine=none',
+            '--template.recipe=chrome-pdf',
+            '--template.content=daily-report-template.html']);
 
     jsReport.stdout.on('data', (data) => {
         console.log(data);
@@ -37,7 +22,8 @@ app.get('/api/report-service', (req, res) => {
     jsReport.on('close', (code) => {
         console.log(`child process exited with code ${code}`);
     });
+    res.send('ok')
 });
 
 
-// const server = app.listen(3000);
+const server = app.listen(3000);

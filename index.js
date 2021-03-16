c00d4070899019cb4e5a4d428633bcc9928136cd
oMid <dehgkaniomid@gmail.com>
2021-03-16 10:23:20 +0330

    update

--- index.js
+++ index.js
@@ -1,31 +1,14 @@
-const express = require('express');
-const { spawn } = require('child_process');
+const { spawn } = require( 'child_process' );
+const ls = spawn( './jsreport', [ 'start' ] );
 
-const app = express();
+ls.stdout.on( 'data', ( data ) => {
+    console.log(data);
+} );
 
-app.get('/api/report-service', (req, res) => {
-    const jsReport = spawn('./jsreport',
-        ['render',
-            `--out=${Date.now()}.pdf`,
-            '--template.engine=none',
-            '--template.recipe=chrome-pdf',
-            '--template.content=daily-report-template.html']);
+ls.stderr.on( 'data', ( data ) => {
+    console.log(data);
+} );
 
-    jsReport.stdout.on('data', (data) => {
-        res.write(data)
-        console.log(data);
-    });
-
-    jsReport.stderr.on('data', (data) => {
-        console.log(data);
-        res.write(data)
-    });
-
-    jsReport.on('close', (code) => {
-        console.log(`child process exited with code ${code}`);
-        res.end(code)
-    });
-});
-
-
-const server = app.listen(3000);
+ls.on( 'close', ( code ) => {
+    console.log( `child process exited with code ${ code }` );
+} );

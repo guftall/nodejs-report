76254f337162fad9af37c43e4be44bec3b378654
oMid <dehgkaniomid@gmail.com>
2021-03-24 21:49:45 +0430

    f

--- index.js
+++ index.js
@@ -24,17 +24,3 @@ fs.writeFile('jsreport.config.json', JSON.stringify(config), 'utf8', () => {
     });
 });
 
-
-const logger = winston.createLogger({
-  level: 'debug',
-  format: winston.format.json(),
-  transports: [
-    new winston.transports.File({ filename: 'combined.log' }),
-  ],
-});
-
-if (process.env.NODE_ENV !== 'production') {
-  logger.add(new winston.transports.Console({
-    format: winston.format.simple(),
-  }));
-}

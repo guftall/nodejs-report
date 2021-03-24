7517b5baef9b73b52f4e5a6c33669f00c9d9d93c
oMid <dehgkaniomid@gmail.com>
2021-03-24 21:58:40 +0430

    listen on 3000

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+const httpProxy = require('http-proxy');
 const { spawn } = require('child_process');
 var fs = require('fs');
 
 let config = require('./jsreport.config.json');
 const { Log } = require('./logger');
 
-config.httpPort = process.env.Port || 3002
+config.httpPort = process.env.Port || 3000
 
 Log.debug('writing back data:', {data: JSON.stringify(config)})
 fs.writeFile('jsreport.config.json', JSON.stringify(config), 'utf8', () => {
@@ -24,3 +25,4 @@ fs.writeFile('jsreport.config.json', JSON.stringify(config), 'utf8', () => {
     });
 });
 
+// httpProxy.createProxyServer({target:'http://localhost:3002'}).listen(3000);
